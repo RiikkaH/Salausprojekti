0e91e3702d395e039d8f211fcc7123d7611bb780
RiikkaH <riikka.huusari@helsinki.fi>
2014-09-09 11:38:50 +0300

    projekti pystyyn

---
+++ Salausprojekti/src/salausprojekti/Salausprojekti.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package salausprojekti;
+
+/**
+ *
+ * @author JuhoRim
+ */
+public class Salausprojekti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
